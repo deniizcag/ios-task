a803bc7d97aac1d2d26e11c3d8f0c1ea933a97ef
DenizCagilligecit <denizcagilligecit@gmail.com>
2021-06-14 00:24:35 +0300

    removed unnecessary constraints

--- CampaignBrowser/Components/LabelWithPadding.swift
+++ CampaignBrowser/Components/LabelWithPadding.swift
@@ -11,11 +11,12 @@ class LabelWithPadding: UILabel {
     @IBInspectable var padding: CGFloat = 8
 
     override func drawText(in rect: CGRect) {
-        super.drawText(in: rect.inset(by: UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)))
+      super.drawText(in: rect.inset(by: UIEdgeInsets(top: 0, left: padding, bottom: 0, right: padding)))
+
     }
 
     override var intrinsicContentSize: CGSize {
         let originalSize = super.intrinsicContentSize
-        return CGSize(width: originalSize.width + padding * 2, height: originalSize.height + padding * 2)
+        return CGSize(width: originalSize.width, height: originalSize.height )
     }
 }
